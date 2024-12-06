88932821cbcf4e54eee5b036a1cd2b077e3490b9
Mike Morrow <mgmorrow8@gmail.com>
2024-12-06 18:02:34 -0500

    Add files via upload

---
+++ Chatbot_test/Chatbot_test/Chatbot_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chatbot_testApp.swift
+//  Chatbot_test
+//
+//  Created by Mike on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Chatbot_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chatbot_test/Chatbot_testTests/Chatbot_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  Chatbot_testTests.swift
+//  Chatbot_testTests
+//
+//  Created by Mike on 12/3/24.
+//
+
+import Testing
+@testable import Chatbot_test
+
+struct Chatbot_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
